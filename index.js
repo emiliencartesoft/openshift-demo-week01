2171acf23ba7d5c7020c800ba601de8875609ebb
Emilien Devos <88321843+emiliencartesoft@users.noreply.github.com>
2021-08-23 16:48:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1><img src="https://i.imgur.com/HV2b6b6.jpg" />')
+  res.send('<h1>Hello from Luxembourg!</h1><img style="height:90%;" src="https://i.imgur.com/HV2b6b6.jpg" />')
 })
 
 app.listen(port, () => {
